4a6d6487b7a817402481a54aeaef97fb4d33d580
apiUi <jan.bouwman@ziggo.nl>
2023-03-01 17:19:19 +0100

    bugfix on MD5, continued automated testing

---
+++ Lazarus/allTypesTesting/runScriptFunctions.ps1
@@ -0,0 +1,2 @@
+echo "run ScriptFunctions"
+..\apiUiServer\apiUiServer --project=.\allTypesResponder.svpr --port=0 --context=localhostapiui --script=runScriptFunctions --terminate
--- Lazarus/allTypesTesting/testApiUiDocker.ps1
+++ Lazarus/allTypesTesting/testApiUiDocker.ps1
@@ -2,6 +2,7 @@ echo "testing..."
 powershell .\startAll.ps1
 powershell .\pushDesigns.ps1
 powershell .\runRequestor.ps1
+powershell .\runScriptFunctions.ps1
 powershell .\stopAll.ps1
 powershell .\simul8rtests.ps1
 powershell .\startApiUi.ps1
